494f738f64666efe30bd1a0617619f5588d9d0ec
ealerto.ph@gmail.com <idel.aranilla@gmail.com>
2025-10-03 21:38:17 +0800

    please let me finish this csp issue

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.use(
           "https://www.ealerto-qcde.com",
           "https://maps.googleapis.com",
           "https://maps.gstatic.com",
-          "https://www.google.com/recaptcha",
-          "https://www.gstatic.com/recaptcha/",
+          "https://www.google.com",
+          "https://www.gstatic.com",
         ],
         "style-src": [
           "'self'",
@@ -67,6 +67,7 @@ app.use(
         "frame-src": [
           "'self'",
           "https://www.google.com",
+          "https://www.gstatic.com",
           "https://maps.googleapis.com",
         ],
         "frame-ancestors": ["'self'"],
