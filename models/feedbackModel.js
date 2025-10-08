b6a65eb95c4dd77ebc0708008ef943bf181964f1
ealerto.ph@gmail.com <idel.aranilla@gmail.com>
2025-10-09 01:30:33 +0800

    fixed feedback final

--- models/feedbackModel.js
+++ models/feedbackModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const feedbackSchema = new mongoose.Schema({
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
   username: { type: String, required: true },
-  report_ID: { type: String, required: true },
+  reportId: { type: String, required: true },
   overall: { type: Number, required: true },
   service: { type: Number, required: true },
   speed: { type: Number, required: true },
